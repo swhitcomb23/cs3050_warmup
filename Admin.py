49b3aca8fb5cd91d1d3ca16d0ac220e8786eea47
Gavin <gusaster76@gmail.com>
2025-09-03 14:54:55 -0400

    uodate admin

--- Admin.py
+++ Admin.py
@@ -1,6 +1,16 @@
+import json
 import firebase_admin
 from firebase_admin import firestore
 
 # Application Default credentials are automatically created.
 app = firebase_admin.initialize_app()
 db = firestore.client()
+
+with open('cool_cars.json') as f:
+    data = json.load(f)
+
+cars = data.get("cool_cars", {})
+for car_id, car_info in cars.items():
+    db.collection("cool_cars").document(car_id).set(car_info)
+
+print('works')

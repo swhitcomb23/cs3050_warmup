80777f6e3b9550509d7d9b3f4383e2093f2f32cb
Sam Wade Whitcomb <swhitcomb23@gmail.com>
2025-09-05 14:49:33 -0400

    updated deliverable#2.txt

--- Query.py
+++ Query.py
@@ -7,6 +7,15 @@ cred = credentials.Certificate('credentials.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+
+# command line looping function
+
+# parsing function that takes in command line
+
+# query function to communicate with database and get data
+
+# pretty printing function?
+
 doc_ref = db.collection("cool_cars").document("car2")
 
 doc = doc_ref.get()

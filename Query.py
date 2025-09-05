95bacabb636334ac31252f4e7ea89f7da8935705
nmckew <143674991+nmckew@users.noreply.github.com>
2025-09-05 14:37:24 -0400

    Start to the Query.py

---
+++ Query.py
@@ -0,0 +1,16 @@
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+cred = credentials.Certificate('credentials.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+doc_ref = db.collection("cool_cars").document("car2")
+
+doc = doc_ref.get()
+if doc.exists:
+    print(f"Document data: {doc.to_dict()}")
+else:
+    print("No such document!")

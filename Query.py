b814d043ed3ae36403f234dc7dbb1ebabc071303
nmckew <143674991+nmckew@users.noreply.github.com>
2025-09-05 14:49:11 -0400

    Added a unfinished Class to Admin

--- Query.py
+++ Query.py
@@ -1,6 +1,7 @@
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+import pyparsing
 
 
 cred = credentials.Certificate('credentials.json')
@@ -8,6 +9,8 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 doc_ref = db.collection("cool_cars").document("car2")
+
+
 
 doc = doc_ref.get()
 if doc.exists:

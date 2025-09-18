e4c6501be5f4a555e75e088db35710f717826c75
Gavin <gusaster76@gmail.com>
2025-09-18 15:24:40 -0400

    edited print function

--- Query.py
+++ Query.py
@@ -83,10 +83,10 @@ class Cool_Car:
 
     def print(self):
         result = ""
-        result += str(self.year) + " " + self.make + " " + self.model
-        if (self.convertible):
+        result += "\n" + str(self.year) + " " + self.make + " " + self.model
+        if self.convertible:
             result += " convertible\n"
-        result += self.transmission + " transmission with " + str(self.BHP) + " horsepower. \n \n"
+        result += self.transmission + " transmission with " + str(self.BHP) + " horsepower. \n"
         print(result)
 
 
